d027f7caf088448cdb5c5dae67707801bd996ef8
Haruwei <Gosha737271@gmail.com>
2024-09-04 14:44:04 +0500

    Добавил проект по с#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 8;
+            int c = 9;
+            int Max = Math.Max(a, b);
+            Max = Math.Max(Max, c);
+            Console.WriteLine(Max);
+            Console.ReadKey();
+        }
+    }
+}
